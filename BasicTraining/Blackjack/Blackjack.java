1f75b67538f3345a8c41b0da32777b4405848c67
Xlocsinjr <Xlocsinjr@gmail.com>
2020-08-31 16:25:07 +0200

    primitive hit command: updates hand and deck arrays

--- BasicTraining/Blackjack/Blackjack.java
+++ BasicTraining/Blackjack/Blackjack.java
@@ -8,13 +8,7 @@ import java.util.Scanner;
 
 public class Blackjack {
   public static void main(String[] args) {
-    /*
-    System.out.println(kaarten.length);
-		Random r = new Random();    // LET OP IMPORTEREN
-		int a = r.nextInt(4);
-    System.out.println(kaarten[a]);
-    */
-    
+    // defines arrays for card array initialisation
     String suit[] = {"Hearts", "Diamonds", "Spades", "Clubs"};
     String value[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
     Card cardArray[] = new Card[suit.length * value.length];
@@ -30,37 +24,75 @@ public class Blackjack {
       }
     }
 
+    // Duplicate the initial (unshuffled array), then shuffles the original 
+    Card cardArrayInit[] = cardArray;
+    cardArray = Card.shuffleDeck(cardArrayInit);
+    Card handArray[] = {};
 
     boolean gameOver = false;
-    Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-    while (!gameOver) {
-      // asks for user input
-      // returns the user given input
+    int points = 0;
 
+    while (!gameOver) {
       System.out.println("Enter a command");
       System.out.println("k: hit    p: pass    r: restart    q: quit");
-
-      
+     
       boolean isValid = false;
-      String userInput = "temp";
+      String userInput = "initInput";
 
       // ask for user input until a valid command has been entered
       while (!isValid) {
+        isValid = true;
+        Scanner scanner = new Scanner(System.in);
         userInput = scanner.nextLine();  // Read user input
 
+        // if hit:
+        // remove the first element in the list of cards
+        // add that card to the hand list
         if (userInput.equals("k")){
-          isValid = true;
+          Card hitCard = cardArray[0];
+          hitCard.declareCard();
+
+          // Updates the hand array: 
+          // creates a new array that's one element longer and adds the new card at the end.
+          int handArrayLength = handArray.length;
+          int newHandLength = handArray.length + 1;
+          Card newHandArray[] = new Card[newHandLength];
+          newHandArray[handArrayLength] = hitCard;
+          handArray = newHandArray;
+
+          // Updates the card array
+          List<Card> newCardList = new ArrayList<Card>(Arrays.asList(cardArray));
+          newCardList.remove(0);	
+          Card[] newCardArray = new Card[newCardList.size()];
+          newCardArray = newCardList.toArray(newCardArray);
+          cardArray = newCardArray;
+
+          System.out.println(handArray.length);
+          System.out.println(cardArray.length);
+
+          
         } 
         else if (userInput.equals("p")){
-          isValid = true;
-        } 
+
+        }
+        // if restarted:
+        // remake the card array to a shuffled list from the initial card array
+        // set points back to 0
         else if (userInput.equals("r")){
-          isValid = true;
+          cardArray = Card.shuffleDeck(cardArrayInit);
+          points = 0;
+          System.out.println("restarting ...");
+          System.out.println("===============================");
         } 
+        // if quit:
+        // end game and code
         else if (userInput.equals("q")){
-          isValid = true;
+          System.out.println("quitting ... To play again, run the code again.");
+          System.out.println("===============================");
+          gameOver = true;
         }
         else {
+          isValid = false;
           System.out.println("Please enter a valid command.");
           System.out.println("k: hit    p: pass    r: restart    q: quit");
         }
@@ -90,28 +122,30 @@ class Card {
     System.out.println(value + " of " + suit);
   }
 
-
-  static List<Card> shuffleDeck(Card[] cardArrayArg){
+  static Card[] shuffleDeck(Card[] cardArrayArg){
     // Shuffle deck https://www.programcreek.com/2012/02/java-method-to-shuffle-an-int-array-with-random-order/
-    //  turns array into list, then shuffles
-    // returns shuffled card list
+    //  turns array into list, shuffles, then turns back into an array
+    // returns shuffled card array
     List<Card> cardList = Arrays.asList(cardArrayArg);
     Collections.shuffle(cardList);
     
-    //Card[] cardArrayNew = new Card[cardList.size()];
-    //cardArrayNew = cardList.toArray(cardArrayNew);
+    Card[] cardArrayNew = new Card[cardList.size()];
+    cardArrayNew = cardList.toArray(cardArrayNew);
 
-    return cardList;
+    return cardArrayNew;
   }
 
-  static List<Card> hit(List<Card> cardListArg){
-    // removes the first element in a list of cards
-    // returns the list with the first element removed
-    System.out.println(cardListArg.get(0));
-    cardListArg.remove(0);	
-    
-    return cardListArg;
+  static int determinePoints(List<Card> handListArg){
+    int handPoints = 0;
+
+    for (int i = 0; i < handListArg.size(); i++){
+      int CardValue = Integer.parseInt(handListArg.get(i).value);
+      
+    }
+
+    return handPoints;
   }
+
 }
 
 
