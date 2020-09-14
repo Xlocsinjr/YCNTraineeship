292f39446d965c753623503185d4c1e2d50bc8c6
Xlocsinjr <Xlocsinjr@gmail.com>
2020-09-14 16:42:36 +0200

    added database context, class and reference

---
+++ BasicTraining/WebApplicationPractice/WebApplicationPracticeDB/Adress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationPracticeDB
+{
+    public class Adress
+    {
+        [Key]
+        public int AdressID { get; set; }
+        public string Street { get; set; }
+        public string AdressNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+    }
+}
